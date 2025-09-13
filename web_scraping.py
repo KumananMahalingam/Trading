157b46d149dbcaff0766d4342463ea345cce2cae
KumananMahalingam <mkumanan@outlook.com>
2025-09-13 14:42:27 -0400

    Used NER model to identify relations between companies

--- web_scraping.py
+++ web_scraping.py
@@ -2,8 +2,13 @@ import finnhub
 import secret_key
 from openpyxl import Workbook
 from datetime import datetime, timezone
+import spacy
+
+from collections import Counter
 
 finnhub_client = finnhub.Client(api_key=secret_key.API_KEY)
+
+nlp = spacy.load("en_core_web_sm")
 
 companies = {
     "AAPL": "Apple",
@@ -41,5 +46,20 @@ for ticker, name in companies.items():
 wb.save("news.xlsx")
 print("News saved to news.xlsx")
 
+
+company_mentions = Counter()
+
+for row in sheet.iter_rows(min_row=2, values_only=True):  
+    headline = row[3]  
+    summary = row[5]  
+    text = f"{headline} {summary}"
+    
+    doc = nlp(text)
+    
+    for ent in doc.ents:
+        if ent.label_ == "ORG": 
+            company_mentions[ent.text] += 1
+
+print(company_mentions)
 
 
